0eea919ef0f44be81abfcb4b7fef832b9253ec6c
Eric R <erkrodc@users.noreply.github.com>
2018-05-30 20:28:40 -0700

    Laid groundwork for coin spewer script. Still needs coin placement/movement, and coin animation.

---
+++ Assets/_Project/Scripts/MonoBehaviour/CoinSpewer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CoinSpewer : MonoBehaviour {
+
+	[SerializeField] private GameObject coinPrefab;
+	private List<GameObject> coinPool = new List<GameObject>();
+	private float delay = 0.1f;
+	private int initialPoolSize = 15;
+
+	private void Awake() {
+		for (int i = 0; i < initialPoolSize; i++) {
+			GameObject clone = Instantiate(coinPrefab, transform);
+			clone.SetActive(false);
+			coinPool.Add(clone);
+		}
+	}
+
+	public void OnGameWin() {
+		StartCoroutine(SpewCoins());
+	}
+
+	private IEnumerator SpewCoins() {
+		WaitForSeconds waitOnDelay = new WaitForSeconds(delay);
+		
+		foreach (GameObject coin in coinPool) {
+			coin.SetActive(true);
+
+			yield return waitOnDelay;
+		}
+	}
+}
