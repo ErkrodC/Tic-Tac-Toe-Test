1faaad90fc3368134c41394fccaf0b1df2d93852
Eric R (ErkrodC) <erkrodc@gmail.com>
2018-06-04 20:50:35 -0700

    Finished coin spewer. Tweaked lighting and post-processing.

---
+++ Assets/_Project/Scripts/MonoBehaviour/CoinBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class CoinBehaviour : MonoBehaviour {
+
+	public bool StartsInBottomLeftCorner; // set by CoinSpewer.cs during coin pool initialization
+	private Vector3 startPosition;
+
+	// initial transform.position is set by CoinSpewer.cs, either to bottomLeft or bottomRight corner of screen.
+	private void Awake() {
+		startPosition = transform.position;
+	}
+	
+	// start animation when gameObject is enabled
+	private void OnEnable() {
+		StartCoroutine(LaunchCoin());
+	}
+
+	// reset position to prepare for next animation 
+	private void OnDisable() {
+		transform.position = startPosition;
+		StopCoroutine(LaunchCoin());
+	}
+
+	// Main coin animation coroutine
+	private IEnumerator LaunchCoin() {
+		float startTime = Time.time;
+		float xVelocity = Random.Range(0.001f, 0.004f);
+		xVelocity *= StartsInBottomLeftCorner ? 1 : -1; 
+
+		// animation loop
+		float timeSinceStart;
+		do {
+			timeSinceStart = Time.time - startTime;
+			// y value is simple distance formula (i.e. y = at^2 + vt)
+			transform.Translate(xVelocity, -.005f * Mathf.Pow(timeSinceStart, 2) + .01f * timeSinceStart, 0);
+			yield return null;
+		} while (timeSinceStart < 3);
+
+		gameObject.SetActive(false);
+	}
+}
--- Assets/_Project/Scripts/MonoBehaviour/CoinSpewer.cs
+++ Assets/_Project/Scripts/MonoBehaviour/CoinSpewer.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
+[RequireComponent(typeof(Canvas))]
 public class CoinSpewer : MonoBehaviour {
 
 	[SerializeField] private GameObject coinPrefab;
+	private Canvas spewerCanvas;
 	private List<GameObject> coinPool = new List<GameObject>();
 	private float delay = 0.1f;
-	private int initialPoolSize = 15;
+	private int initialPoolSize = 25;
+	private Vector3 bottomLeftCorner, bottomRightCorner;
 
 	private void Awake() {
-		for (int i = 0; i < initialPoolSize; i++) {
-			GameObject clone = Instantiate(coinPrefab, transform);
-			clone.SetActive(false);
-			coinPool.Add(clone);
-		}
+		spewerCanvas = GetComponent<Canvas>();
+
+		InitializeCornerPositions();
+		InitializeCoinPool();
+		
+		
 	}
 
 	public void OnGameWin() {
@@ -30,5 +33,22 @@ public class CoinSpewer : MonoBehaviour {
 
 			yield return waitOnDelay;
 		}
+	}
+
+	private void InitializeCoinPool() {	
+		for (int i = 0; i < initialPoolSize; i++) {
+			GameObject clone = Instantiate(coinPrefab, i % 2 == 0 ? bottomLeftCorner : bottomRightCorner, Quaternion.identity, transform);
+			clone.GetComponent<CoinBehaviour>().StartsInBottomLeftCorner = i % 2 == 0; 
+			clone.SetActive(false);
+			coinPool.Add(clone);
+		}
+	}
+
+	private void InitializeCornerPositions() {
+		Vector3[] spewerCanvasCorners = new Vector3[4];
+		(spewerCanvas.transform as RectTransform)?.GetWorldCorners(spewerCanvasCorners);
+
+		bottomLeftCorner = spewerCanvasCorners[0];
+		bottomRightCorner = spewerCanvasCorners[3];
 	}
 }
